6ba40542b90924aff4a6cf36ee7d311e5b8ab871
yoshisaka <y_yutaka@fa2.so-net.ne.jp>
2020-11-05 16:48:18 +0900

    OpenCV 4.5.0の使用に切り替え

--- projects/MovieExporter/win_vs2017/run_shade3d_v17_with_OpenCV.bat
+++ projects/MovieExporter/win_vs2017/run_shade3d_v17_with_OpenCV.bat
@@ -1,4 +1,4 @@
-set OPENCV_PATH=C:\WinApp\OpenCV-4.4.0
+set OPENCV_PATH=C:\WinApp\OpenCV-4.5.0
 set PATH=%OPENCV_PATH%\build\x64\vc15\bin;%PATH%
 
 set Shade3D_APP=C:\Program Files\Shade3D\Shade3D ver.17
